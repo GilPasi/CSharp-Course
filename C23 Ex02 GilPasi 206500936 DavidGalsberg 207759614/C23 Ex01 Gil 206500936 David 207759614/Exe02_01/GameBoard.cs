130c4fbe5f9c8e5b31162029eef15816ff207f07
Gil Pasi <gilpasi@Gils-MacBook-Pro.local>
2023-08-05 10:35:38 +0300

    git test

--- C23 Ex02 GilPasi 206500936 DavidGalsberg 207759614/C23 Ex01 Gil 206500936 David 207759614/Exe02_01/GameBoard.cs
+++ C23 Ex02 GilPasi 206500936 DavidGalsberg 207759614/C23 Ex01 Gil 206500936 David 207759614/Exe02_01/GameBoard.cs
@@ -1,6 +1,12 @@
-namespace Exe02_01;
 
-public class GameBoard
+namespace Exe02_01
 {
-    
+    public class GameBoard
+    {
+        public static void PrintState()
+        {
+            Console.WriteLine();
+        }
+    }
 }
+
